5466d9e2b173ec44ffed29c88840660161452b05
youssefwt <Ywtalaat@gmail.com>
2022-06-26 21:18:12 +0200

    registeratin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./app.scss";
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 const App = () => {
   return (
     <>
       {/* <Home /> */}
-      <Watch />
+      {/* <Watch /> */}
+      <Register />
     </>
   );
 };
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from "react";
+import "./register.scss";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value);
+  };
+
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://raw.githubusercontent.com/youssefwt/Movie-Mania/main/logo.png"
+            alt=""
+          />
+          <button className="loginButton">Sign in</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Subscribe
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Register;
