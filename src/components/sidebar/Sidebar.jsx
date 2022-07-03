68fe65b9e6c7ebfa65d034892c18f847bd2ebd26
Azhar Elamyr <azharelamyr@gmail.com>
2022-07-03 11:28:45 +0200

    updating style

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,11 +5,69 @@ import {
   PlayCircleFilledOutlined,
   RssFeed,
 } from "@mui/icons-material";
+import { Add, Remove } from "@mui/icons-material";
+
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../../authContext/AuthContext";
 
 import { Users } from "../../dummyData";
-import CloseFriend from "../closeFriend/CloseFriend";
 
-export default function Sidebar() {
+import CloseFriend from "../closeFriend/CloseFriend";
+import axios from "axios";
+export default function Sidebar({ user }) {
+  const [followers, setfollowers] = useState([]);
+  const { user: currentUser, dispatch } = useContext(AuthContext);
+  console.log(user);
+  const [followed, setFollowed] = useState(
+    currentUser.followings.includes(user?.id)
+  );
+  useEffect(() => {
+    //get friends
+    const getFriends = async () => {
+      try {
+        const followersList = await axios.get("http://localhost:8800/users", {
+          headers: { token: `Bearer ${user.accessToken}` },
+        });
+        setfollowers(followersList.data);
+        console.log(followersList.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getFriends();
+  }, [user]);
+
+  const handleClick = async () => {
+    try {
+      if (followed) {
+        await axios.put(
+          `http://localhost:8800/users/${user._id}/unfollow`,
+          {
+            userId: currentUser._id,
+          },
+          //
+          {
+            headers: { token: `Bearer ${user.accessToken}` },
+          }
+        );
+        dispatch({ type: "UNFOLLOW", payload: user._id });
+      } else {
+        await axios.put(
+          `http://localhost:8800/users/${user._id}/follow`,
+          {
+            userId: currentUser._id,
+          },
+          //
+          {
+            headers: { token: `Bearer ${user.accessToken}` },
+          }
+        );
+        dispatch({ type: "FOLLOW", payload: user._id });
+      }
+      setFollowed(!followed);
+    } catch (err) {}
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebarWrapper ">
@@ -33,11 +91,21 @@ export default function Sidebar() {
         </ul>
         <button className="sidebarButton"> Show More</button>
         <hr className="sidebarHr" />
-        <ul className="sidebarFriendLList">
-          {Users.map((u) => (
-            <CloseFriend key={u.id} user={u} />
-          ))}
-        </ul>
+        <div className="sidebarLiftContainer">
+          <ul className="sidebarFriendLList">
+            {followers.map((u) => (
+              <div key={u._id}>
+                <CloseFriend user={u} />
+                <div className="sidebarFollow">
+                  <button className="sidebarFollowButton" onClick={handleClick}>
+                    {followed ? "Unfollow" : "Follow"}
+                    {followed ? <Remove /> : <Add />}
+                  </button>
+                </div>
+              </div>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/pages/newsFeed/NewsFeed.jsx
+++ src/pages/newsFeed/NewsFeed.jsx
@@ -5,12 +5,28 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
 import "./NewsFeed.css";
 
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
+import { useParams } from "react-router";
+
 export default function NewsFeed() {
+  const [user, setUser] = useState({});
+  const username = useParams().username;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=${username}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [username, user.accessToken]);
   return (
     <>
       <Topbar />
       <div className="NewsFeedContainer">
-        <Sidebar />
+        <Sidebar user={user} />
         <Feed />
       </div>
     </>
